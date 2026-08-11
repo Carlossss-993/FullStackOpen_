ef9d9cd0f202b28992a9010fa6e27d9c2d7b6443
Carlos Fandiño <csfandinoh@udistrital.edu.co>
2026-08-10 21:46:30 -0500

    I've completed part1 exercise3.

--- part1/courseInformation/src/Content.jsx
+++ part1/courseInformation/src/Content.jsx
@@ -4,9 +4,9 @@ const Content = (props) => {
     
     return (
         <>
-            <Part part={props.content[0].part} exercises={props.content[0].exercises}/>
-            <Part part={props.content[1].part} exercises={props.content[1].exercises}/>
-            <Part part={props.content[2].part} exercises={props.content[2].exercises}/>
+            <Part part={props.content[0].name} exercises={props.content[0].exercises}/>
+            <Part part={props.content[1].name} exercises={props.content[1].exercises}/>
+            <Part part={props.content[2].name} exercises={props.content[2].exercises}/>
         </>
     )
 }
