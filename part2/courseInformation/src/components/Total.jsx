6826f82377a6ae75580c6f9c1477f8d5595745a7
Carlos Fandiño <csfandinoh@udistrital.edu.co>
2026-08-11 12:59:50 -0500

    Part2 Exercise1

---
+++ part2/courseInformation/src/components/Total.jsx
@@ -0,0 +1,12 @@
+const Total = (props) => {
+    
+    let total = 0
+    props.parts.forEach(part => total+=part.exercises)
+    return (
+        <p>
+            Number of exercises {total}
+        </p>
+    )
+}
+
+export default Total
