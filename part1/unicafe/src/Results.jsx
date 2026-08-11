ea3a2b4f4c57f8070820ca5bc66f494404cba963
Carlos Fandiño <csfandinoh@udistrital.edu.co>
2026-08-11 00:19:12 -0500

    part1 exercise7

--- part1/unicafe/src/Results.jsx
+++ part1/unicafe/src/Results.jsx
@@ -1,8 +1,17 @@
-const Results = ({ good, neutral, bad }) => 
-    <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-    </div>
+const Results = ({ good, neutral, bad }) => {
+
+    const total = good + neutral + bad
+
+    return (
+        <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>All: {total}</p>
+            <p>Average: {total === 0 ? 0 : (good-bad)/(total)}</p>
+            <p>Positive: {total === 0 ? 0 : good/(total)}</p>
+        </div>
+    )
+}
 
 export default Results
