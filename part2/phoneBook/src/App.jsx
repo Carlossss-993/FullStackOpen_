7e48b611101241e221552e06029cd6413b2d4abe
Carlos Fandiño <csfandinoh@udistrital.edu.co>
2026-08-11 20:44:19 -0500

    part2 exercise9

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [personsToShow, setPersonsToShow] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleInputName = (event) => {
     setNewName(event.target.value)
@@ -15,6 +28,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleInputFilter = (event) => {
+    setFilter(event.target.value)
+    const newPersons = persons.filter((person) => person.name.toLowerCase().startsWith(event.target.value.toLowerCase()))
+    
+    if (event.target.value === '') {
+      setPersonsToShow(persons)
+    } else {
+      setPersonsToShow(newPersons)
+    }
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
     if (persons.some((person) => person.name === newName)) {
@@ -22,8 +46,10 @@ const App = () => {
       setNewName('')
     } else {
       setPersons(persons.concat({name: newName, number: newNumber}))
+      setPersonsToShow(persons.concat({name: newName, number: newNumber}))
       setNewName('')
       setNewNumber('')
+      setFilter('')
     }
   }
 
@@ -42,7 +68,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}: {person.number}</p>)}
+      Filter: name starts with <input value={filter} onChange={handleInputFilter} />
+      {personsToShow.map((person) => <p key={person.name}>{person.name}: {person.number}</p>)}
     </div>
   )
 }
