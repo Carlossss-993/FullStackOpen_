9fcb610f26705f70d405b182c7b7ba3d3a4ffedf
Carlos Fandiño <csfandinoh@udistrital.edu.co>
2026-08-20 12:15:44 -0500

    part4 exercise5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,13 +1,19 @@
-const dummy = (blogs) => {
-  console.log(blogs.length)
-  return 1
-}
+const dummy = () => 1
 
-const totalLikes = (blogs) =>
+const totalLikes = blogs =>
   blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) {return null}
+  return blogs.reduce((favoritedBlog, blog) => {
+    return blog.likes > favoritedBlog.likes
+      ? { title: blog.title, author: blog.author, likes: blog.likes }
+      : favoritedBlog
+  }, { title: blogs[0].title, author: blogs[0].author, likes: blogs[0].likes })
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
